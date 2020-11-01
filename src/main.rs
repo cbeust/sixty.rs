a374d5c731f4eff2b8466ecb7b0b3ad9c2814708
Cedric Beust <cedric@beust.com>
2020-10-31 19:24:36 -0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,16 @@ fn main() {
 }
 
 use std::io::prelude::*;
-use std::io;
-use rand::Rng;
 use std::fs::File;
 
-use std::cmp::Ordering;
+fn word(buffer: &Vec<u8>, index: usize) -> u16 {
+    return buffer[index + 1] as u16 | ((buffer[index + 2] as u16) << 8);
+}
 
 fn sixty() {
     let mut buffer: Vec<u8> = Vec::new();
     let mut f = File::open("6502_functional_test.bin").expect("Couldn't find the file");
     f.read_to_end(&mut buffer);
-    let start = buffer[0x400];
 
     let mut i: u16 = 0x400;
     while i < 0x600 {
@@ -28,36 +27,9 @@ fn sixty() {
         } else if size == 2 {
             println!("{:04X}: {} {:02X}", i, name, buffer[index + 1]);
         } else {
-            let word: u16 = buffer[index + 1] as u16 | ((buffer[index + 2] as u16) << 8);
-            println!("{:04X}: {} {:X}", i, name, word);
+            println!("{:04X}: {} {:X}", i, name, word(&buffer, index));
         }
         i = i + size as u16;
     }
     println!("Read file");
-}
-
-fn game() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
 }
