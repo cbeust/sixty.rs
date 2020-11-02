89d311b90a319a39ab5f5c848cf35dcc379cc5e7
Cedric Beust <cedric@beust.com>
2020-11-01 18:04:39 -0800

    Cpu and Memory

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,47 @@ fn main() {
     sixty();
 }
 
+trait Memory {
+    fn get(& mut self, index: usize) -> u8;
+    fn set(& mut self, index: usize, value: u8);
+}
+
+struct SimpleMemory {
+    buffer: [u8; 0x10000],
+}
+
+impl Memory for SimpleMemory {
+    fn get(& mut self, index: usize) -> u8 {
+        self.buffer[index]
+    }
+
+    fn set(& mut self, index: usize, value: u8) {
+        self.buffer[index] = value
+    }
+}
+
+struct Cpu<'a> {
+    memory: &'a Memory,
+    a: u8,
+    x: u8,
+    y: u8,
+    pc: usize,
+    sp: usize,
+}
+
+impl<'a> Cpu<'a> {
+    fn new(mem: &'a Memory) -> Cpu<'a> {
+        Cpu {
+            memory: mem,
+            a: 0,
+            x: 0,
+            y: 0,
+            pc: 0,
+            sp: 0,
+        }
+    }
+}
+
 fn word(buffer: &Vec<u8>, index: usize) -> u16 {
     return buffer[index + 1] as u16 | ((buffer[index + 2] as u16) << 8);
 }
@@ -17,6 +58,11 @@ fn sixty() {
     let mut f = File::open("6502_functional_test.bin").expect("Couldn't find the file");
     f.read_to_end(&mut buffer);
 
+    let mem = SimpleMemory{
+        buffer: [0;0x10000]
+    };
+
+    let cpu = Cpu::new(&mem);
     let mut i: usize = 0x600;
     while i < 0x700 {
         let (s, size) = disassemble(&buffer, i);
