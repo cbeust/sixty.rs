62edfdc81f588fad718cce328dddc84f0d0e28e5
Cedric Beust <cedric@beust.com>
2020-11-01 16:48:02 -0800

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod constants;
 
+use constants::*;
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
     sixty();
 }
-
-use std::io::prelude::*;
-use std::fs::File;
 
 fn word(buffer: &Vec<u8>, index: usize) -> u16 {
     return buffer[index + 1] as u16 | ((buffer[index + 2] as u16) << 8);
@@ -24,20 +25,24 @@ fn sixty() {
     }
 }
 
-use constants::*;
-
 fn disassemble(buffer: &Vec<u8>, index: usize) -> (String, usize) {
     let opcode = buffer[index] as usize;
     let name = constants::OPCODE_NAMES[opcode];
     let size: usize = constants::SIZES[opcode];
-    let addressingType = &ADDRESSING_TYPES[opcode];
+    let addressing_type = &ADDRESSING_TYPES[opcode];
 
     let result: String = match size {
-        1 => format!("{:04X}: {}", index, name),
-        2 => format!("{:04X}: {} {}", index, name,
-             addressingType.to_string(index, buffer[index + 1], 0)),
-        _ => format!("{:04X}: {} {}", index, name,
-            addressingType.to_string(index, buffer[index + 1], word(&buffer, index)))
+        1 => format!("{:04X}: {:02X}         {}", index,
+                     opcode,
+                     name),
+        2 => format!("{:04X}: {:02X} {:02X}      {} {}", index,
+                     opcode, buffer[index + 1],
+                     name,
+                     addressing_type.to_string(index, buffer[index + 1], 0)),
+        _ => format!("{:04X}: {:02X} {:02X} {:02X}   {} {}", index,
+                     opcode, buffer[index + 1], buffer[index + 2],
+                     name,
+                     addressing_type.to_string(index, buffer[index + 1], word(&buffer, index)))
     };
 
     return (result, size);
