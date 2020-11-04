f42b041bb17d227fc6d532affa4224a448021971
Cedric Beust <cedric@beust.com>
2020-11-04 00:52:47 -0800

    StatusFlags

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,8 @@
+#![allow(unused)]
+#![allow(warnings)]
+
 use crate::{Memory, constants::*};
+use std::fmt;
 
 pub struct Cpu {
     pub memory: Box<dyn Memory>,
@@ -7,6 +11,62 @@ pub struct Cpu {
     pub y: u8,
     pub pc: usize,
     pub sp: usize,
+    pub p: StatusFlags
+}
+
+pub struct StatusFlags {
+    value: u8
+}
+
+impl StatusFlags {
+    fn new() -> StatusFlags {
+        StatusFlags { value: 0 }
+    }
+
+    fn set(&mut self, v: u8) {
+        self.value = v;
+    }
+
+    fn get_bit(&self, bit: u8) -> bool {
+        self.value & (1 << bit) != 0
+    }
+
+    fn set_bit(&mut self, f: bool, bit: u8) {
+        if f { self.value = self.value | (1 << bit) }
+        else { self.value = self.value & !(1 << bit) }
+    }
+
+    fn n(&self) -> bool { self.get_bit(7) }
+    fn set_n(&mut self, f: bool) { self.set_bit(f, 7) }
+    fn v(&self) -> bool { self.get_bit(6) }
+    fn set_v(&mut self, f: bool) { self.set_bit(f, 6) }
+    fn b(&self) -> bool { self.get_bit(4) }
+    fn set_b(&mut self, f: bool) { self.set_bit(f, 4) }
+    fn d(&self) -> bool { self.get_bit(3) }
+    fn set_d(&mut self, f: bool) { self.set_bit(f, 3) }
+    fn i(&self) -> bool { self.get_bit(2) }
+    fn set_i(&mut self, f: bool) { self.set_bit(f, 2) }
+    fn z(&self) -> bool { self.get_bit(1) }
+    fn set_z(&mut self, f: bool) { self.set_bit(f, 1) }
+    fn c(&self) -> bool { self.get_bit(0) }
+    fn set_c(&mut self, f: bool) { self.set_bit(f, 0) }
+}
+
+impl fmt::Display for StatusFlags {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fn s(n: &str, v: bool) -> &str {
+            if v {n} else {"-".as_ref()}
+        }
+
+        write!(f, "{{P:${:02X} {}{}-{}{}{}{}{}}}", self.value,
+               s("N", self.n()),
+               s("V", self.v()),
+               s("B", self.b()),
+               s("D", self.d()),
+               s("I", self.i()),
+               s("Z", self.z()),
+               s("C", self.c()))
+    }
 }
 
 impl Cpu {
@@ -18,10 +78,13 @@ impl Cpu {
             y: 0,
             pc: 0,
             sp: 0,
+            p: StatusFlags::new()
         }
     }
 
     pub fn run(&mut self, pc: usize) {
+        self.p.set(0xff);
+        println!("Current p: {}", self.p);
         self.pc = pc;
         let max = 10;
         let mut i = 0;
@@ -31,7 +94,8 @@ impl Cpu {
             println!("{}", s);
             // let addressing_type = &ADDRESSING_TYPES[opcode];
             match opcode {
-                CLD => { println!("CLD") },
+                CLD => self.p.set_d(false),
+                SED => self.p.set_d(true),
                 // BRK => break,
                 _ => { println!("Unknown opcode: {:2X}", opcode) }
             }
